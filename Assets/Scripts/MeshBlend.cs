94b2a93870afc027d96feb5ba0b43c4905ba10a1
Aurelien Tavernier <aurelientav@gmail.com>
2024-03-22 14:11:44 +0100

    MeshBlend anim script

--- Assets/Scripts/MeshBlend.cs
+++ Assets/Scripts/MeshBlend.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class MeshBlend : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private SkinnedMeshRenderer _skinnedMeshRenderer;
+    private Mesh _skinnedMesh;
+    private int _blendShapeCount;
+
+    private int _playIndex;
+
     void Start()
     {
-        
+        _skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+        _skinnedMesh = GetComponent<Mesh>();
+        _blendShapeCount = _skinnedMesh.blendShapeCount;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (_playIndex > 0)
+        {
+            _skinnedMeshRenderer.SetBlendShapeWeight(_playIndex - 1, 0);
+        }
         
+        if (_playIndex == 0)
+        {
+            _skinnedMeshRenderer.SetBlendShapeWeight(_blendShapeCount - 1, 0);
+        }
+
+        _skinnedMeshRenderer.SetBlendShapeWeight(_playIndex, 100);
+        _playIndex++;
+
+        if (_playIndex > _blendShapeCount - 1)
+        {
+            _playIndex = 0;
+        }
     }
 }

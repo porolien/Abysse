13a151a1be4ea30490788911c4e1795754137e93
Aurelien Tavernier <aurelientav@gmail.com>
2024-03-22 14:45:49 +0100

    new fish

--- Assets/Scripts/Fish/FishMain.cs
+++ Assets/Scripts/Fish/FishMain.cs
@@ -7,6 +7,8 @@ public class FishMain : MonoBehaviour
     public FishMovement movement;
     public PatrolPath patrol;
 
+    public bool afraidByLight;
+
     private void Awake()
     {
         SendMessage("Init", this);
